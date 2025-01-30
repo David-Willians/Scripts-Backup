669a0955a544e0a3c847c7148c08de3da6f7d92d
Davi <rascunhodacharge@gmail.com>
2025-01-30 07:30:32 -0300

    Scripts

---
+++ Muscle legends/Muscle open.lua
@@ -0,0 +1,51 @@
+--[[
+
+
+██████╗ ██╗     ██╗   ██╗████████╗ ██████╗ ███╗   ██╗██╗██╗   ██╗███╗   ███╗   ██╗     ██╗   ██╗ █████╗ 
+██╔══██╗██║     ██║   ██║╚══██╔══╝██╔═══██╗████╗  ██║██║██║   ██║████╗ ████║   ██║     ██║   ██║██╔══██╗
+██████╔╝██║     ██║   ██║   ██║   ██║   ██║██╔██╗ ██║██║██║   ██║██╔████╔██║   ██║     ██║   ██║███████║
+██╔═══╝ ██║     ██║   ██║   ██║   ██║   ██║██║╚██╗██║██║██║   ██║██║╚██╔╝██║   ██║     ██║   ██║██╔══██║
+██║     ███████╗╚██████╔╝   ██║   ╚██████╔╝██║ ╚████║██║╚██████╔╝██║ ╚═╝ ██║██╗███████╗╚██████╔╝██║  ██║
+╚═╝     ╚══════╝ ╚═════╝    ╚═╝    ╚═════╝ ╚═╝  ╚═══╝╚═╝ ╚═════╝ ╚═╝     ╚═╝╚═╝╚══════╝ ╚═════╝ ╚═╝  ╚═╝
+ 
+                                                                                                       
+--]]
+
+--[[
+    ////////////////////////////////////////
+    //   © 2023 - 2024 Plutonium.lua      //
+    //       All rights reserved          //
+    ////////////////////////////////////////
+    //     This material may not be       //
+    //     reproduced, displayed,         //
+    //     modified, or distributed       //
+    //     without the express prior      //
+    //     written permission of the      //
+    //         copyright holder.          //
+    ////////////////////////////////////////
+]]
+
+--// • MAIN DETAILS • \\ --w
+
+--// • Owner: PawsThePaw
+--// • Obfuscated: Yes
+--// • Version: 3.7.3
+--// • Libraries & Modules: 6
+--// • Developement Speed: Very Slow
+--// • Discord Server: https://discord.com/invite/rGVUA8Hp
+--// • Developers: PawsThePaw
+--// • Average Updates Per Week: 1 - 3
+--// • Average Load Time: 0 - 10 Seconds
+--// • Average System Downtimes: N/A
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/PawsThePaw/Plutonium.AA/refs/heads/main/Versions%202/V3.7.4.txt", true))()
+
+
+game.StarterGui:SetCore(
+        "SendNotification",
+        {
+            Title = "Plutonium.lua",
+            Text = "Blade Ball is under repair please be patient!!",
+            Duration = 60
+        }
+    )
